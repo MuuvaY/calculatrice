fd9842832aaab0128d41117290c93364f741c2de
Marius YVART <mariusyvt@gmail.com>
2024-09-22 19:02:46 +0200

    design améliorer

--- src/index.js
+++ src/index.js
@@ -8,8 +8,28 @@ for (let boutons = 1; boutons < 10; boutons++) {
 
   btndiv.append(bouton);
 }
+const operator = document.getElementById("btnoperator");
 
-const text = document.getElementById("btndiv");
+const addition = document.createElement("button");
+const soustraction = document.createElement("button");
+const multiplication = document.createElement("button");
+const division = document.createElement("button");
+
+addition.textContent = "+";
+addition.className = "operator";
+
+soustraction.textContent = "-";
+soustraction.className = "operator";
+
+multiplication.textContent = "x";
+multiplication.className = "operator";
+
+division.textContent = "/";
+division.className = "operator";
+
+operator.append(addition, soustraction, multiplication, division);
+
+const text = document.getElementById("calculatrice");
 
 text.addEventListener("click", calculate);
 
